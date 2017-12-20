2d07ca47cda52528778e76d4e9d75c36a22ad480
Bill <blevien@seaburyhall.org>
2017-12-19 16:03:49 -1000

    Initial Commit

---
+++ Downloader/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Downloader
+//
+//  Created by Bill Levien on 12/19/17.
+//  Copyright © 2017 Bill Levien. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
